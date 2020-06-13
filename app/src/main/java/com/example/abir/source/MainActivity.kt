c5f0a4ae7e073cd6685b7f54c5329df7cd9c4bcb
Abir Hasan <abirhasan89@gmail.com>
2020-06-14 00:27:40 +0600

    PropertyAnimationActivity.kt : Initial Implementation

--- app/src/main/java/com/example/abir/source/MainActivity.kt
+++ app/src/main/java/com/example/abir/source/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.abir.source.sample.file_download_retrofit.FileDownloadActivity
 import com.example.abir.source.sample.guided_tutorial.GuideActivity
+import com.example.abir.source.sample.property_animation.PropertyAnimationActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -13,6 +14,15 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         setButton1()
         setButton2()
+        setButton3()
+    }
+
+    private fun setButton1() {
+        button1.setOnClickListener {
+            Intent(this, FileDownloadActivity::class.java).apply {
+                startActivity(this)
+            }
+        }
     }
 
     /**
@@ -29,9 +39,9 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun setButton1() {
-        button1.setOnClickListener {
-            Intent(this, FileDownloadActivity::class.java).apply {
+    private fun setButton3() {
+        button3.setOnClickListener {
+            Intent(this, PropertyAnimationActivity::class.java).apply {
                 startActivity(this)
             }
         }
--- app/src/main/java/com/example/abir/source/sample/property_animation/PropertyAnimationActivity.kt
+++ app/src/main/java/com/example/abir/source/sample/property_animation/PropertyAnimationActivity.kt
@@ -1,12 +1,35 @@
 package com.example.abir.source.sample.property_animation
 
-import androidx.appcompat.app.AppCompatActivity
+import android.animation.ValueAnimator
 import android.os.Bundle
+import android.view.animation.AccelerateDecelerateInterpolator
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.example.abir.source.R
+import kotlinx.android.synthetic.main.activity_property_animation.*
+
 
 class PropertyAnimationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_property_animation)
+
+        btn1.setOnClickListener {
+            Toast.makeText(this, "Clicked", Toast.LENGTH_SHORT).show()
+        }
+
+        propertyAnimationWithValueAnimator()
+    }
+
+    private fun propertyAnimationWithValueAnimator() {
+        val valueAnimator: ValueAnimator = ValueAnimator.ofFloat(0f, 500f)
+        valueAnimator.interpolator =
+            AccelerateDecelerateInterpolator() // increase the speed first and then decrease
+
+        valueAnimator.duration = 2000
+        valueAnimator.addUpdateListener { animation ->
+            btn1.translationY = animation.animatedValue.toString().toFloat()
+        }
+        valueAnimator.start()
     }
 }
