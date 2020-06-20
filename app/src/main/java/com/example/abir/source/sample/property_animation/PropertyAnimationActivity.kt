b6c84faf158b71cac2f7224c7fdedb3547cb25aa
Abir Hasan <abirhasan89@gmail.com>
2020-06-20 19:25:35 +0600

    PropertyAnimation with ObjectAnimator

--- app/src/main/java/com/example/abir/source/sample/property_animation/PropertyAnimationActivity.kt
+++ app/src/main/java/com/example/abir/source/sample/property_animation/PropertyAnimationActivity.kt
@@ -1,24 +1,51 @@
 package com.example.abir.source.sample.property_animation
 
+import android.animation.ObjectAnimator
 import android.animation.ValueAnimator
 import android.os.Bundle
 import android.view.animation.AccelerateDecelerateInterpolator
+import android.view.animation.BounceInterpolator
+import android.view.animation.LinearInterpolator
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.abir.source.R
 import kotlinx.android.synthetic.main.activity_property_animation.*
 
 
+/**
+ * They are used to alter property of objects (Views or non view objects).
+ * We specify certain properties(like translateX, TextScaleX) of the objects to change.
+ * Various characteristics of animation which can be manipulated are animation duration,
+ * whether to reverse it and for how many times we want to repeat the animation etc
+ */
 class PropertyAnimationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_property_animation)
 
         btn1.setOnClickListener {
-            Toast.makeText(this, "Clicked", Toast.LENGTH_SHORT).show()
+            Toast.makeText(this, "Clicked 1", Toast.LENGTH_SHORT).show()
+        }
+
+        btn2.setOnClickListener {
+            Toast.makeText(this, "Clicked 2", Toast.LENGTH_SHORT).show()
         }
 
         propertyAnimationWithValueAnimator()
+        propertyAnimationWithObjectAnimator()
+    }
+
+    /**
+     * Same animation as [propertyAnimationWithValueAnimator] just using the ObjectAnimator Subclass
+     * Also on a different view with a few different parameters
+     */
+    private fun propertyAnimationWithObjectAnimator() {
+        val objectAnimator: ObjectAnimator = ObjectAnimator.ofFloat(
+            btn2, "translationY", 0f, -500f
+        )
+        objectAnimator.duration = 2000
+        objectAnimator.interpolator = BounceInterpolator()
+        objectAnimator.start()
     }
 
     private fun propertyAnimationWithValueAnimator() {
