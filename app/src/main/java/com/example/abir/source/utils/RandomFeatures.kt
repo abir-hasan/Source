e266b3b15fe9c49b4fe9e4f4ef97a01f40789e25
Abir Hasan <abirhasan89@gmail.com>
2021-07-27 21:37:36 +0600

    Opening Facebook Messenger page of an user(with id) if app is installed.

---
+++ app/src/main/java/com/example/abir/source/utils/RandomFeatures.kt
@@ -0,0 +1,46 @@
+package com.example.abir.source.utils
+
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.widget.Toast
+import com.example.abir.source.MainActivity
+
+
+fun MainActivity.openFbMessengerOfAUser(userId: String = "186202548075080") {
+    try {
+        val messengerUrl: String = if (
+            isAppInstalled("com.facebook.orca") ||
+            isAppInstalled("com.facebook.mlite")
+        ) {
+            "fb-messenger://user/$userId/"
+        } else {
+            Toast.makeText(
+                this,
+                "FB-Messenger is not installed , open Messenger in browser",
+                Toast.LENGTH_SHORT
+            ).show()
+            "https://www.messenger.com/t/$userId/"
+        }
+        val messengerIntent = Intent(Intent.ACTION_VIEW)
+        messengerIntent.data = Uri.parse(messengerUrl)
+        startActivity(messengerIntent)
+    } catch (e: Exception) {
+        e.printStackTrace()
+    }
+}
+
+fun MainActivity.isAppInstalled(packageName: String): Boolean {
+    return try {
+        this.applicationContext.packageManager.getApplicationInfo(
+            // Make sure to put this in manifest queries for Android 11(API-30) and above
+            // Doc: https://developer.android.com/training/package-visibility
+            packageName,
+            0
+        )
+        true
+    } catch (e: PackageManager.NameNotFoundException) {
+        e.printStackTrace()
+        false
+    }
+}
