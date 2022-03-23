6cad95f13325242ed19dc0eca324351b0b8e01ba
Abir Hasan <abirhasan89@gmail.com>
2022-03-24 02:09:12 +0600

    Blurring with input parameter

--- app/src/main/java/com/example/abir/source/sample/work_manager_sample/BlurViewModel.kt
+++ app/src/main/java/com/example/abir/source/sample/work_manager_sample/BlurViewModel.kt
@@ -22,7 +22,8 @@ import android.content.Context
 import android.net.Uri
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import androidx.work.OneTimeWorkRequest
+import androidx.work.Data
+import androidx.work.OneTimeWorkRequestBuilder
 import androidx.work.WorkManager
 import com.example.abir.source.R
 import com.example.abir.source.sample.work_manager_sample.workers.BlurWorker
@@ -38,12 +39,28 @@ class BlurViewModel(application: Application) : ViewModel() {
     init {
         imageUri = getImageUri(application.applicationContext)
     }
+
     /**
      * Create the WorkRequest to apply the blur and save the resulting image
      * @param blurLevel The amount to blur the image
      */
     internal fun applyBlur(blurLevel: Int) {
-        workmanager.enqueue(OneTimeWorkRequest.from(BlurWorker::class.java))
+        val blurRequest = OneTimeWorkRequestBuilder<BlurWorker>()
+            .setInputData(createInputDataForUri())
+            .build()
+        workmanager.enqueue(blurRequest)
+    }
+
+    /**
+     * Creates the input data bundle which includes the Uri to operate on
+     * @return Data which contains the Image Uri as a String
+     */
+    private fun createInputDataForUri(): Data {
+        val builder = Data.Builder()
+        imageUri?.let {
+            builder.putString(KEY_IMAGE_URI, imageUri.toString())
+        }
+        return builder.build()
     }
 
     private fun uriOrNull(uriString: String?): Uri? {
--- app/src/main/java/com/example/abir/source/sample/work_manager_sample/workers/BlurWorker.kt
+++ app/src/main/java/com/example/abir/source/sample/work_manager_sample/workers/BlurWorker.kt
@@ -2,26 +2,43 @@ package com.example.abir.source.sample.work_manager_sample.workers
 
 import android.content.Context
 import android.graphics.BitmapFactory
+import android.net.Uri
+import android.text.TextUtils
 import androidx.work.Worker
 import androidx.work.WorkerParameters
-import com.example.abir.source.R
+import androidx.work.workDataOf
+import com.example.abir.source.sample.work_manager_sample.KEY_IMAGE_URI
 import com.example.abir.source.utils.logError
 
 class BlurWorker(context: Context, params: WorkerParameters) : Worker(context, params) {
 
-    companion object{
+    companion object {
         private const val TAG = "BlurWorker"
     }
 
     override fun doWork(): Result {
         val appContext = applicationContext
 
+        val resourceUri = inputData.getString(KEY_IMAGE_URI)
+
         makeStatusNotification("Blurring image", appContext)
 
         return try {
-            val picture = BitmapFactory.decodeResource(
-                appContext.resources,
-                R.drawable.android_cupcake)
+            // REMOVE THIS
+            //    val picture = BitmapFactory.decodeResource(
+            //            appContext.resources,
+            //            R.drawable.android_cupcake)
+
+            if (TextUtils.isEmpty(resourceUri)) {
+                "Invalid input uri".logError(TAG)
+                throw IllegalArgumentException("Invalid input uri")
+            }
+
+            val resolver = appContext.contentResolver
+
+            val picture = BitmapFactory.decodeStream(
+                resolver.openInputStream(Uri.parse(resourceUri))
+            )
 
             val output = blurBitmap(picture, appContext)
 
@@ -30,7 +47,9 @@ class BlurWorker(context: Context, params: WorkerParameters) : Worker(context, p
 
             makeStatusNotification("Output is $outputUri", appContext)
 
-            Result.success()
+            val workOutput = workDataOf(KEY_IMAGE_URI to outputUri.toString())
+
+            Result.success(workOutput)
         } catch (throwable: Throwable) {
             "doWork() Error applying blur".logError(TAG)
             Result.failure()
