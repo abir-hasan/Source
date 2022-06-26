ffeb9706a7ff536954cd04369e81bd33411fd703
Abir Hasan Shawon <abirhasan89@gmail.com>
2022-06-26 17:38:07 +0200

    Achieved unit tests for CarShould.kt as it is not dependent on Engine class anymore. Cause we have mocked everything properly.

--- app/src/test/java/com/example/abir/source/unit_test/example_five/unittests/CarShould.kt
+++ app/src/test/java/com/example/abir/source/unit_test/example_five/unittests/CarShould.kt
@@ -6,8 +6,11 @@ import com.example.abir.source.unit_test.example_five.MainCoroutineScopeRule
 import com.nhaarman.mockitokotlin2.mock
 import com.nhaarman.mockitokotlin2.times
 import com.nhaarman.mockitokotlin2.verify
+import com.nhaarman.mockitokotlin2.whenever
 import junit.framework.TestCase.assertEquals
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.test.runTest
 import org.junit.Rule
 import org.junit.Test
@@ -16,7 +19,24 @@ import org.junit.Test
 class CarShould {
 
     private val engine: EngineV2 = mock()
-    private val car = Car(engine, 5.0)
+    private val car: Car
+
+    init {
+        // Whenever we make an object, we also need to mock the behavior of all of its functions.
+        // Now, when the function does not have a return type, then there is nothing to mock.
+        runTest {
+            whenever(engine.turnOn()).thenReturn(flow {
+                delay(2000)
+                emit(25)
+                delay(2000)
+                emit(50)
+                delay(2000)
+                emit(95)
+            })
+        }
+        // Mock the behaviour of the dependency first. Then create the object
+        car = Car(engine, 5.0)
+    }
 
     @get:Rule
     var coroutinesTestRule = MainCoroutineScopeRule()
